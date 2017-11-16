ec922f18ca5ad69a78bc1dbdbb66462104c1bf88
felixerus <felixerus@gmail.com>
2017-11-15 16:21:23 -0800

    Find unique vowels in given list

--- Chapter 2/vowels.py
+++ Chapter 2/vowels.py
@@ -1,5 +1,9 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
+found = []
 word = 'Milliways'
 for letter in word:
     if letter in vowels:
-        print(letter)
+        if letter not in found:
+            found.append(letter)
+for vowel in found:
+    print(vowel)
