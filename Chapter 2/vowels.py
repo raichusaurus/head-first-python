5885b5360346558bc18985136852b2ff059fcae6
felixerus <felixerus@gmail.com>
2017-11-15 16:22:53 -0800

    Remove hard-coded word for user interaction

--- Chapter 2/vowels.py
+++ Chapter 2/vowels.py
@@ -1,6 +1,6 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 found = []
-word = 'Milliways'
+word = input("Provide a word to search for vowels: ")
 for letter in word:
     if letter in vowels:
         if letter not in found:
