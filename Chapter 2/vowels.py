1afbb0c6b853109086e022463b013ffd5383d5f8
felixerus <felixerus@gmail.com>
2017-11-15 16:29:48 -0800

    Optimize search through vowels

--- Chapter 2/vowels.py
+++ Chapter 2/vowels.py
@@ -3,7 +3,7 @@ found = []
 word = input("Provide a word to search for vowels: ")
 for letter in word:
     if letter in vowels:
-        if letter not in found:
-            found.append(letter)
+        found.append(letter)
+        vowels.remove(letter)
 for vowel in found:
     print(vowel)
