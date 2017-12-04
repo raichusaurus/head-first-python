f84662dbd4a1f1d9f93dd1fd5767c987f7cc5003
felixerus <felixerus@gmail.com>
2017-12-04 15:45:33 -0800

    Take advantage of ".setdefault()" to initialize key values

--- Chapter 3/vowels.py
+++ Chapter 3/vowels.py
@@ -5,8 +5,7 @@ word = input("Provide a word to search for vowels: ")
 
 for letter in word:
     if letter in vowels:
-        if letter not in found:
-            found[letter] = 0
+        found.setdefault(letter, 0)
         found[letter] += 1
 
 for k, v in sorted(found.items()):
