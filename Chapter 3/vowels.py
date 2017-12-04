92d1224c61d21959885439b8fd1fc886a0b5afb6
felixerus <felixerus@gmail.com>
2017-12-04 15:44:00 -0800

    Take advantage of "not in" with dictionary lookup

--- Chapter 3/vowels.py
+++ Chapter 3/vowels.py
@@ -5,10 +5,9 @@ word = input("Provide a word to search for vowels: ")
 
 for letter in word:
     if letter in vowels:
-        if letter in found:
-            found[letter] += 1
-        else:
-            found[letter] = 1
+        if letter not in found:
+            found[letter] = 0
+        found[letter] += 1
 
 for k, v in sorted(found.items()):
     print(k, 'was found', v, 'time(s).')
