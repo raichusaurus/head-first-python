5059e48ff416bea16784a40d9a07040eba0202c7
felixerus <felixerus@gmail.com>
2017-12-04 15:42:27 -0800

    Only display found vowels

--- Chapter 3/vowels.py
+++ Chapter 3/vowels.py
@@ -1,13 +1,14 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 found = {}
-for vowel in vowels:
-    found[vowel] = 0
 
 word = input("Provide a word to search for vowels: ")
 
 for letter in word:
     if letter in vowels:
-        found[letter] += 1
+        if letter in found:
+            found[letter] += 1
+        else:
+            found[letter] = 1
 
 for k, v in sorted(found.items()):
     print(k, 'was found', v, 'time(s).')
