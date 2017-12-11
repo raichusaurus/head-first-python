b4ed94c5ec7529ab39ef245845d20095c5424083
felixerus <felixerus@gmail.com>
2017-12-11 12:07:01 -0800

    Reuse search_for_letters() in search_for_vowels()

--- Chapter 4/vowels.py
+++ Chapter 4/vowels.py
@@ -1,7 +1,7 @@
 def search_for_vowels(phrase:str) -> set:
     """Return any vowels in a supplied phrase."""
     vowels = set('aeiou')
-    return vowels.intersection(set(phrase))
+    return search_for_letters(phrase, vowels)
 
 
 def search_for_letters(phrase:str, letters:str) -> set:
