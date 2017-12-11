ccf5337e2e9b6281eca4dcbe090f74e2b0b168f9
felixerus <felixerus@gmail.com>
2017-12-11 15:52:02 -0800

    PEP8

--- Chapter 4/vowels.py
+++ Chapter 4/vowels.py
@@ -1,9 +1,9 @@
-def search_for_vowels(phrase:str) -> set:
+def search_for_vowels(phrase: str) -> set:
     """Return any vowels in a supplied phrase."""
-    vowels = set('aeiou')
+    vowels = 'aeiou'
     return search_for_letters(phrase, vowels)
 
 
-def search_for_letters(phrase:str, letters:str='aeiou') -> set:
+def search_for_letters(phrase: str, letters: str='aeiou') -> set:
     """Return any supplied letters found from the supplied phrase."""
     return set(letters).intersection(set(phrase))
