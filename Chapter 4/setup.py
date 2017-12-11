c6d80d35ea5a73de8b21093fa55defe86fa57bde
felixerus <felixerus@gmail.com>
2017-12-11 15:32:04 -0800

    Share vowels module using interpreter's site-packages locations

---
+++ Chapter 4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vowels',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='John Hightshue',
+    author_email='john.hightshue@gmail.com',
+    url='https://github.com/raichusaurus/head-first-python/tree/master/Chapter%204',
+    py_modules=['vowels'],
+)
