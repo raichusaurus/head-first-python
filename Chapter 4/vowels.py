729b66f71619102991d174fe4b8983a04e83586b
felixerus <felixerus@gmail.com>
2017-12-11 12:04:48 -0800

    Add new search_for_letters function

--- Chapter 4/vowels.py
+++ Chapter 4/vowels.py
@@ -2,3 +2,8 @@ def search_for_vowels(phrase:str) -> set:
     """Return any vowels in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
+
+
+def search_for_letters(phrase:str, letters:str) -> set:
+    """Return any supplied letters found from the supplied phrase."""
+    return set(letters).intersection(set(phrase))
