bdac26a2d71b55837f00aadda2ea95106d76383c
felixerus <felixerus@gmail.com>
2017-12-11 11:06:28 -0800

    Standardize for PEP8

--- Chapter 4/vowels.py
+++ Chapter 4/vowels.py
@@ -1,7 +1,7 @@
-def searchForVowels():
+def search_for_vowels():
     """Display any vowels found in an asked-for word."""
     vowels = set('aeiou')
-    word = input("Provide a word to search for vowels: ")
+    word = input('Provide a word to search for vowels: ')
     found = vowels.intersection(set(word))
     for vowel in found:
         print(vowel)
