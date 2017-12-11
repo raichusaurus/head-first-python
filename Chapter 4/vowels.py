e0e55780f06126921c85fbc74bd9a4acf57d3e9f
felixerus <felixerus@gmail.com>
2017-12-11 11:40:06 -0800

    Take word argument to search

--- Chapter 4/vowels.py
+++ Chapter 4/vowels.py
@@ -1,7 +1,6 @@
-def search_for_vowels():
+def search_for_vowels(word):
     """Display any vowels found in an asked-for word."""
     vowels = set('aeiou')
-    word = input('Provide a word to search for vowels: ')
     found = vowels.intersection(set(word))
     for vowel in found:
         print(vowel)
