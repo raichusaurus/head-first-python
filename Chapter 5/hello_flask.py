0bddb91eeaae999f793fefd4f9d1f735a790bb36
felixerus <felixerus@gmail.com>
2017-12-21 13:36:45 -0800

    First webapp in Python using Flask

--- Chapter 5/hello_flask.py
+++ Chapter 5/hello_flask.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from vowels import search_for_letters
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hell() -> str:
+def hello() -> str:
     return 'Hello world from Flask!'
+
+
+@app.route('/search')
+def search_for() -> str:
+    return str(search_for_letters('life, the universe, and everything!', 'eiru'))
+
 
 app.run()
