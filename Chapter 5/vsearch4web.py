d0899e9cfb9143ee788a80ac0b56d9101c84db3d
felixerus <felixerus@gmail.com>
2017-12-21 15:10:45 -0800

    Refactor vsearch

--- Chapter 5/vsearch4web.py
+++ Chapter 5/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from vowels import search_for_letters
 
 app = Flask(__name__)
@@ -12,6 +12,11 @@ def hello() -> str:
 @app.route('/search')
 def search_for() -> str:
     return str(search_for_letters('life, the universe, and everything!', 'eiru'))
+
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search for letters on the web!')
 
 
 app.run()
